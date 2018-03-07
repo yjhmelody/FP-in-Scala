d8a79d28c7754823c5ec213953514f4b2abdc7ef
yjhmelody <465402634@qq.com>
2018-03-07 19:53:39 +0800

    update readme

--- practice/2.2.scala
+++ practice/2.2.scala
@@ -7,9 +7,18 @@ def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     }
     
     loop(0, 1);
+
+    // @annotation.tailrec
+    // def go(n: Int): Boolean =
+    //     if (n >= as.length-1) true
+    //     else if (gt(as(n), as(n+1))) false
+    //     else go(n+1)
+    // go(0)
+
 }
 
-val as: Array[Int] = Array[Int](1,3,5)
+// val as: Array[Int] = Array[Int](1,3,5)
+val as = Array(1,3,5)
 
 println(isSorted(as, (prev: Int, cur: Int) => prev < cur))
 println(isSorted(as, (prev: Int, cur: Int) => prev > cur))
