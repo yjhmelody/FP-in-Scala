4ae2ad5808e0d3be417dfc9f83f3782890e35e4f
yjhmelody <465402634@qq.com>
2018-03-12 14:56:49 +0800

    ch04: check 4.1

--- practice/ch04.scala
+++ practice/ch04.scala
@@ -9,25 +9,35 @@ trait Option[+A] {
         case Some(x) => Some(f(x))
     }
 
+    def getOrElse[B >: A](default: => B): B = this match {
+        case None => default
+        case Some(x) => x
+    }
+
     def flatMap[B](f: A => Option[B]): Option[B] = this match {
         case None => None
         case Some(x) => f(x)
     }
 
-    def getOrElse[B >: A](default: => B): B = this match {
-        case None => default
-        case Some(x) => x
-    }
+    def flatMap2[B](f: A => Option[B]): Option[B] = 
+        map(f) getOrElse None
+
 
     def orElse[B >: A](ob: Option[B]): Option[B] = this match {
         case None => ob
         case _ => this
     }
 
+    def orElse2[B >: A](ob: Option[B]): Option[B] = 
+        this map (Some(_)) getOrElse ob
+
     def filter(f: A => Boolean): Option[A] = this match {
-        case Some(x) if(f(x)) => Some(x)
+        case Some(x) if f(x) => this
         case _ => None
     }
+
+    def filter2(f: A => Boolean): Option[A] =
+        flatMap(a => if(f(a)) Some(a) else None)
 }
 
 val xs1: Option[Int] = Some(1)
@@ -38,13 +48,15 @@ println(xs1.map(_ * 2))
 println(xs2.map(_ * 2))
 
 println(xs3.flatMap((x) => Some(x + 2)))
+println(xs3.flatMap2((x) => Some(x + 2)))
+
 println(xs1.getOrElse(233))
 println(xs2.getOrElse(() => 233))
 
 println(xs1.orElse(Some(233)))
-println(xs2.orElse(Some(233)))
+println(xs2.orElse2(Some(233)))
 
 println(xs1.filter(_ == 2))
-println(xs2.filter(_ == 2))
+println(xs2.filter2(_ == 2))
 println(xs3.filter(_ == 2))
 println("4.1")
